222015abe41bcf0c468ab71744bc9a4fe0277be0
Beatriz <biabagg12@gmail.com>
2024-10-18 11:50:42 -0300

    feat: add user constructor

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -6,18 +6,29 @@ const Home = () => {
 
   const [user, setUser] = useState<UserProps | null>(null);
 
-  const loadUser = async(userName: string) => {
+  const loadUser = async (userName: string) => {
 
     const response = await fetch(`https://api.github.com/users/${userName}`);
 
     const data = await response.json();
 
-    console.log(data);
-  }
+    const { avatar_url, login, location, followers, following } = data;
+
+    const userData: UserProps = {
+      avatar_url,
+      login,
+      location,
+      followers,
+      following,
+    };
+
+    setUser(userData);
+  };
 
   return (
     <div>
       <Search loadUser={loadUser} />
+      {user && <p>{user.login}</p>}
     </div>
   )
 }
