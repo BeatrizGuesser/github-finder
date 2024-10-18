9f5e0b0fa0d0d69b1ce5d52d947ac424c7240ac8
Beatriz <biabagg12@gmail.com>
2024-10-18 10:25:22 -0300

    feat: add GitHub API request

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,13 +1,24 @@
 import { BsSearch } from "react-icons/bs"
+import { useState } from "react";
 
-const Search = () => {
+type SearchProps = {
+    loadUser: (userName: string) => Promise<void>;
+};
+
+const Search = ({ loadUser }: SearchProps) => {
+
+    const [userName, setUserName] = useState("");
     return (
         <div>
             <h2>Search for a user:</h2>
             <p>Know your best repositories</p>
             <div>
-                <input type="text" placeholder="Enter the username" />
-                <button>
+                <input
+                    type="text"
+                    placeholder="Enter the username"
+                    onChange={(e) => setUserName(e.target.value)}
+                />
+                <button onClick={() => loadUser(userName)}>
                     <BsSearch />
                 </button>
             </div>
--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,9 +1,23 @@
 import Search from "../components/Search"
+import { UserProps } from "../types/user";
+import { useState } from "react";
 
 const Home = () => {
+
+  const [user, setUser] = useState<UserProps | null>(null);
+
+  const loadUser = async(userName: string) => {
+
+    const response = await fetch(`https://api.github.com/users/${userName}`);
+
+    const data = await response.json();
+
+    console.log(data);
+  }
+
   return (
     <div>
-      <Search />
+      <Search loadUser={loadUser} />
     </div>
   )
 }
