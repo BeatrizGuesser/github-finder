95e0bc6b9f81039ee9d827bd40ee89f536e10ebb
Beatriz <biabagg12@gmail.com>
2024-10-18 16:43:05 -0300

    feat: add repo GitHub API request

--- src/routes/Repos.tsx
+++ src/routes/Repos.tsx
@@ -3,15 +3,51 @@ import { useParams } from 'react-router-dom'
 
 import styles from "./Repos.module.css"
 import BackBtn from '../components/BackBtn'
+import { RepoProps } from '../types/repo'
+import Loader from '../components/Loader'
 
 const Repos = () => {
 
     const { username } = useParams();
+    const [repos, setRepos] = useState<RepoProps[] | [] | null>(null);
+    const [isLoading, setIsLoading] = useState(false);
+
+    useEffect(() => {
+
+        const loadRepos = async function (username: string) {
+
+            setIsLoading(true);
+
+            const response = await fetch(`https://api.github.com/users/${username}/repos`);
+
+            const data = await response.json();
+
+            setIsLoading(false);
+
+            setRepos(data);
+            
+        }
+
+        if (username) {
+            loadRepos(username);
+        }
+
+    }, [])
+
+    if (!repos && isLoading) return <Loader />;
 
     return (
         <div>
             <BackBtn />
-            Repos {username}
+            <h2>Explore user repositories: {username}</h2>
+            {repos && repos.length === 0 && <p>There are no repositories.</p>}
+            {repos && repos.length > 0 && (
+                <div>
+                    {repos.map((repo: RepoProps) => (
+                        <p>{repo.name}</p>
+                    ))}
+                </div>
+            )}
         </div>
     )
 }
---
+++ src/types/repo.ts
@@ -0,0 +1,7 @@
+export type RepoProps = {
+    name: string,
+    html_url: string;
+    language: string;
+    startgazers_count: number;
+    forks_count: number;
+};
